d7537356fd433814e274292bcc0511a48aada7ea
Alain Gascon <alain.g@opendeusto.es>
2025-11-06 21:46:02 +0100

    Panel Autentificacion

--- src/domain/Usuario.java
+++ src/domain/Usuario.java
@@ -1,11 +1,14 @@
 package domain;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
-public class Usuario {
+public class Usuario implements Serializable{
 	
 
+
+	private static final long serialVersionUID = 1L;
 	private String nombre;
 	private String apellidos;
 	private String dni;
